2625fb2ba2cf737b27406dc2e7f1aae79adc34cb
Ludovic HENRY <ludovic.henry@me.com>
2020-02-15 19:57:19 -0400

    Terminé la conformité MVVM

--- CountOnMe/Model/Calculator.swift
+++ CountOnMe/Model/Calculator.swift
@@ -8,7 +8,55 @@
 
 import Foundation
 
-struct Calculator {
-    var textString: String
-    var errorMessage: String
+class Calculator {
+    var display: String
+    
+    init() {
+        self.display = ""
+    }
+    
+    var elements: [String] {
+        return display.split(separator: " ").map { "\($0)" }
+    }
+    
+    var expressionIsCorrect: Bool {
+        return elements.last != "+" && elements.last != "-"
+    }
+    
+    var expressionHaveEnoughElement: Bool {
+        return elements.count >= 3
+    }
+    
+    var canAddOperator: Bool {
+        return elements.last != "+" && elements.last != "-"
+    }
+    
+    var expressionHaveResult: Bool {
+        return display.firstIndex(of: "=") != nil
+    }
+    
+    func calculateResult() {
+        
+        var operationsToReduce = elements
+        
+        // Iterate over operations while an operand still here
+        while operationsToReduce.count > 1 {
+            let left = Int(operationsToReduce[0])!
+            let operand = operationsToReduce[1]
+            let right = Int(operationsToReduce[2])!
+            
+            let result: Int
+            switch operand {
+            case "+": result = left + right
+            case "-": result = left - right
+            default: fatalError("Unknown operator !")
+            }
+            
+            operationsToReduce = Array(operationsToReduce.dropFirst(3))
+            operationsToReduce.insert("\(result)", at: 0)
+        }
+        display.append(" = \(operationsToReduce.first!)")
+    }
+    
+    
 }
--- CountOnMe/View Model/CalculatorViewModel.swift
+++ CountOnMe/View Model/CalculatorViewModel.swift
@@ -19,53 +19,31 @@ class CalculatorViewModel {
         
     private var textString: String {
         didSet {
+            calculator.display = textString
             delegate?.displayText(textString)
         }
     }
     
-    private var errorMessage: String {
+    private var errorMessage: String = "" {
         didSet {
-            calculator.errorMessage = self.errorMessage
             delegate?.displayError(errorMessage)
         }
     }
 
     init(calculator: Calculator) {
         self.calculator = calculator
-        self.errorMessage = calculator.errorMessage
-        self.textString = calculator.textString
+        self.textString = ""
     }
-    
-    private var elements: [String] {
-        return textString.split(separator: " ").map { "\($0)" }
-    }
-    
-    // Error check computed variables
-    private var expressionIsCorrect: Bool {
-        return elements.last != "+" && elements.last != "-"
-    }
-    
-    private var expressionHaveEnoughElement: Bool {
-        return elements.count >= 3
-    }
-    
-    private var canAddOperator: Bool {
-        return elements.last != "+" && elements.last != "-"
-    }
-    
-    private var expressionHaveResult: Bool {
-        return textString.firstIndex(of: "=") != nil
-    }
-        
+            
     func tappedNumber(number: String) {
-        if expressionHaveResult {
+        if calculator.expressionHaveResult {
             textString = ""
         }
         textString.append(number)
     }
     
     func tappedAddition() {
-        if canAddOperator {
+        if calculator.canAddOperator {
             textString.append(" + ")
         } else {
             errorMessage = "Un operateur est déja mis !"
@@ -73,7 +51,7 @@ class CalculatorViewModel {
     }
     
     func tappedminus() {
-        if canAddOperator {
+        if calculator.canAddOperator {
             textString.append(" - ")
         } else {
             errorMessage = "Un operateur est déja mis !"
@@ -81,37 +59,19 @@ class CalculatorViewModel {
     }
 
     func tappedequal() {
-        guard expressionIsCorrect else {
+        guard calculator.expressionIsCorrect else {
             errorMessage = "Entrez une expression correcte !"
             return
         }
         
-        guard expressionHaveEnoughElement else {
+        guard calculator.expressionHaveEnoughElement else {
             errorMessage = "Démarrez un nouveau calcul !"
             return
         }
         
-        // Create local copy of operations
-        var operationsToReduce = elements
+        calculator.calculateResult()
+        textString = calculator.display
         
-        // Iterate over operations while an operand still here
-        while operationsToReduce.count > 1 {
-            let left = Int(operationsToReduce[0])!
-            let operand = operationsToReduce[1]
-            let right = Int(operationsToReduce[2])!
-            
-            let result: Int
-            switch operand {
-            case "+": result = left + right
-            case "-": result = left - right
-            default: fatalError("Unknown operator !")
-            }
-            
-            operationsToReduce = Array(operationsToReduce.dropFirst(3))
-            operationsToReduce.insert("\(result)", at: 0)
-        }
-        
-        textString.append(" = \(operationsToReduce.first!)")
     }
 
     
